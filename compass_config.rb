1581ac0bc77720c7c2e5fda61292613d49334615
Devin <ho.devin05@gmail.com>
2016-10-19 14:58:27 -0500

    mp4_client_starter foundation update

--- compass_config.rb
+++ compass_config.rb
@@ -1,4 +1,5 @@
 # Find out more at http://compass-style.org/help/documentation/configuration-reference/
+add_import_path "public/lib/foundation-sites/scss"
 css_dir = 'public/css'
 sass_dir = 'source_sass'
 javascripts_dir = 'source_js'
