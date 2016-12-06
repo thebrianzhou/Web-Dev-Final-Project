4bd1dd0cab0387b264a7585e52e6a481b8ea136a
thebrianzhou <bwzhou2@illinois.edu>
2016-12-06 16:28:27 -0600

    fixed kicking out auth

--- front_end/source_js/app.js
+++ front_end/source_js/app.js
@@ -61,7 +61,7 @@ app.config(['$routeProvider', function($routeProvider) {
 
 app.run(['$rootScope', '$location', 'authentication', function($rootScope, $location, authentication){
   $rootScope.$on('$routeChangeStart', function(event, nextRoute, currentRoute) {
-      if ($location.path() === '/user' && !authentication.isLoggedIn()) {
+      if (!authentication.isLoggedIn()) {
         $location.path('/splashpage');
       }
     });
